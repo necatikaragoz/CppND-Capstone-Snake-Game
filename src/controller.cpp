b35d20f3ba38f4a83bf9427da2f93184667c97a0
Necati <necati_karagoz@hotmail.com>
2021-06-28 01:47:36 +0300

    wasd buttons are added to controller class

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,6 +3,13 @@
 #include "SDL.h"
 #include "snake.h"
 
+
+/*
+  These files define the Controller class. 
+  This class handles keyboard input using the SDL libary, 
+  and it sets the snake's direction based on the input.
+*/
+
 void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
                                  Snake::Direction opposite) const {
   if (snake.direction != opposite || snake.size == 1) snake.direction = input;
@@ -17,21 +24,25 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
         case SDLK_UP:
+        case SDLK_w:
           ChangeDirection(snake, Snake::Direction::kUp,
                           Snake::Direction::kDown);
           break;
 
         case SDLK_DOWN:
+        case SDLK_s:
           ChangeDirection(snake, Snake::Direction::kDown,
                           Snake::Direction::kUp);
           break;
 
         case SDLK_LEFT:
+        case SDLK_a:
           ChangeDirection(snake, Snake::Direction::kLeft,
                           Snake::Direction::kRight);
           break;
 
         case SDLK_RIGHT:
+        case SDLK_d:
           ChangeDirection(snake, Snake::Direction::kRight,
                           Snake::Direction::kLeft);
           break;
