f6bb7f27c33dc0539154e9c973921c7909d05145
necatikaragoz <necati_karagoz@hotmail.com>
2021-06-27 11:28:16 +0300

    message queue class is added

---
+++ src/MessageQueue.cpp
@@ -0,0 +1,41 @@
+#include <MessageQueue.h>
+
+/* Implementation of class "MessageQueue" */
+
+template <typename T>
+T MessageQueue<T>::receive()
+{
+    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // to wait for and receive new messages and pull them from the queue using move semantics. 
+    // The received object should then be returned by the receive function. 
+    
+    // perform queue modification under the lock
+    std::unique_lock<std::mutex> uLock(_mutex);
+
+    _cond.wait(uLock, [this] { return !_messages.empty(); }); // pass unique lock to condition variable
+
+    // remove last vector element from queue
+    T msg = std::move(_messages.back());
+    _messages.pop_back();
+
+    return msg; // will not be copied due to return value optimization (RVO) in C++
+
+}
+
+template <typename T>
+void MessageQueue<T>::send(T &&msg)
+{
+    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+
+    // simulate some work
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // perform vector modification under the lock
+    std::lock_guard<std::mutex> uLock(_mutex);
+
+    // add vector to queue
+    std::cout << "   Message " << msg << " has been sent to the queue" << std::endl;
+    _messages.push_back(std::move(msg));
+    _cond.notify_one(); // notify client after pushing new Vehicle into vector
+}
